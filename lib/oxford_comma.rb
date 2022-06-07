b2f0827b6128811194ddc1e38462ac79f805f056
Eli Craig <elidcraig@gmail.com>
2022-06-06 18:56:31 -0700

    commit tree 02a4b9053a38a9cc2594bf260d0f82d1ff7bfe18

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+# require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(' and ')
+  else  
+    array[array.length - 1] = 'and ' + array.last
+    array.join(', ')
+  end  
 end
+
+# binding.pry
